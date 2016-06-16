5c1b4e12fc01c0064246f80904bd6775f920bcc9
JasonMrX <jiasenxu@gmail.com>
2016-06-16 13:22:46 -0400

    basic checker and annotation

---
+++ src/intrange/IntRangeChecker.java
@@ -0,0 +1,7 @@
+package intrange;
+
+import org.checkerframework.common.basetype.BaseTypeChecker;
+
+public class IntRangeChecker extends BaseTypeChecker{
+
+}
---
+++ src/intrange/qual/IntRange.java
@@ -0,0 +1,5 @@
+package intrange.qual;
+
+public @interface IntRange {
+
+}
