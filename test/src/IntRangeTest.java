603f895968221f27ef78d3db25c2a01240d42162
JasonMrX <jiasenxu@gmail.com>
2016-06-21 17:45:10 -0400

    add checkerFrameworkTest

---
+++ test/src/IntRangeTest.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+import org.checkerframework.framework.test.CheckerFrameworkTest;
+import org.junit.runners.Parameterized.Parameters;
+
+public class IntRangeTest extends CheckerFrameworkTest{
+
+	public IntRangeTest(File testFile) {
+		super(testFile,
+				intrange.IntRangeChecker.class,
+				"",
+				"-Anomsgtext");
+	}
+	
+	@Parameters
+	public static String[] getTestDirs() {
+		return new String[]{};
+	}
+	
+}
