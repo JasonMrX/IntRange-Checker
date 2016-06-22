ae23faacc978a30d8f0b4007ec32fe26552f856f
JasonMrX <jiasenxu@gmail.com>
2016-06-22 16:20:15 -0400

    tune last test case

--- tests/intrange-visitor/WarnFromGreaterThanTo.java
+++ tests/intrange-visitor/WarnFromGreaterThanTo.java
@@ -2,10 +2,11 @@ import intrange.qual.*;
 
 public class WarnFromGreaterThanTo {
 
-    //:: warning: (from(200).greater.than.to(100))
-	void testMethod(@IntRange(from=200, to=100) int eir) {
-        // test warning of error intrange qualifier
-        @IntRange(from=0, to=255) int j = eir; 
+	void testMethod(@IntRange(from=0, to=255) int eir, //OK
+		    //:: warning: (from(200).greater.than.to(100))
+			@IntRange(from=200, to=100) int a, //warning
+			@IntRange(from=150, to=150) int b //OK
+			) {
 	}
 	
 }
