abf3f82e2dc866d16f3186f9dffecc2d5161b133
JasonMrX <jiasenxu@gmail.com>
2016-07-05 16:56:49 -0400

    refine last test case and find bugs

--- tests/controlFlow/NumericalOperationTests.java
+++ tests/controlFlow/NumericalOperationTests.java
@@ -3,20 +3,26 @@ import intrange.qual.*;
 public class NumericalOperationTests {
 
     void testMethod(
-            @IntRange(from=0, to=10) int a,
-            @IntRange(from=10, to=20) int b) {
+            @IntRange(from=-5, to=5) int a,
+            @IntRange(from=10, to=20) int b,
+            @IntRange(from=-20, to=-10) int c
+          ) {
         
         /* + */
-        @IntRange(from=11, to=30) int plus1 = a + b; // error
-        @IntRange(from=10, to=30) int plus2 = a + b; //OK
+        @IntRange(from=6, to=25) int plus1 = a + b; // error
+        @IntRange(from=5, to=25) int plus2 = a + b; //OK
         
         /* - */
-        @IntRange(from=-20, to=-1) int minus1 = a - b; //error
-        @IntRange(from=-20, to=0) int minus2 = a - b; //OK
+        @IntRange(from=-25, to=-6) int minus1 = a - b; //error
+        @IntRange(from=-25, to=-5) int minus2 = a - b; //OK
         
         /* * */
-        @IntRange(from=1, to=200) int mult1 = a * b; //error
-        @IntRange(from=0, to=200) int mult2 = a * b; //OK
+        @IntRange(from=-100, to=99) int mult1 = a * b; //error
+        @IntRange(from=-100, to=100) int mult2 = a * b; //OK
+        @IntRange(from=-100, to=99) int mult3 = a * c; //error
+        @IntRange(from=-100, to=100) int mult4 = a * c; //OK
+        @IntRange(from=-400, to=-101) int mult5 = b * c; //error
+        @IntRange(from=-400, to=-100) int mult6 = b * c; //OK
         
         /**
          * /
