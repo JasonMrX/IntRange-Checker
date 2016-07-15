e60be5469068d996018d922e247e8e9df13fb9e5
JasonMrX <jiasenxu@gmail.com>
2016-07-14 20:54:19 -0400

    one more test case for aliased annotation

--- tests/intrange/AliasedAnnotations.java
+++ tests/intrange/AliasedAnnotations.java
@@ -1,7 +1,12 @@
+import android.support.annotation.*;
 class AliasedAnnotations {
     
     void useIntRangeAnnotation() {
         //:: error: (assignment.type.incompatible)
         @android.support.annotation.IntRange(from=0, to=10) int i = 12;
+
+        //:: error: (assignment.type.incompatible)
+        @IntRange(from=0, to=10) int j = 13;
+        
     }
 }
