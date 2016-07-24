f0553bae7cfac1a4257ff41e4f3b46a1e3dea98f
JasonMrX <jiasenxu@gmail.com>
2016-07-23 23:56:39 -0400

    modified test class according to the newly refactored test framework

--- tests/src/IntRangeTest.java
+++ tests/src/IntRangeTest.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.util.List;
 
-import org.checkerframework.framework.test.CheckerFrameworkTest;
+import org.checkerframework.framework.test.CheckerFrameworkPerFileTest;
+import org.checkerframework.framework.test.TestUtilities;
 import org.junit.runners.Parameterized.Parameters;
 
-public class IntRangeTest extends CheckerFrameworkTest{
+public class IntRangeTest extends CheckerFrameworkPerFileTest{
 
     public IntRangeTest(File testFile) {
         super(testFile,
@@ -13,8 +15,8 @@ public class IntRangeTest extends CheckerFrameworkTest{
     }   
     
     @Parameters
-    public static String[] getTestDirs() {
-        return new String[]{"intrange"};
-    }   
+       public static List<File> getTestFiles() {
+         return TestUtilities.findNestedJavaTestFiles("intrange");
+       }  
     
 }
