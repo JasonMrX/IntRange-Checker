e8e88005b88ba2075605eb243695d8ae93b4ea06
JasonMrX <jiasenxu@gmail.com>
2016-07-23 23:56:51 -0400

    add desugarings test

---
+++ tests/intrange/Desugarings.java
@@ -0,0 +1,28 @@
+import intrange.qual.IntRange;
+
+class Desugarings {
+    
+    public void postfix() {
+        int i = 3;
+        @IntRange(from=3, to=3) int a = i++;
+        @IntRange(from=4, to=4) int b = i;
+        @IntRange(from=4, to=4) int c = i--;
+        
+    }
+    
+    public void prefix() {
+        int i = 3;
+        @IntRange(from=4, to=4) int a = ++i;
+        @IntRange(from=4, to=4) int b = i;
+        @IntRange(from=3, to=3) int c = --i;
+    }
+    
+    public void compoundAssignments() {
+        int i = 3;
+        @IntRange(from=6, to=6) int a = (i += 3);
+        @IntRange(from=3, to=3) int b = (i -= 3);
+        @IntRange(from=9, to=9) int c = (i *= 3);
+        @IntRange(from=3, to=3) int d = (i /= 3);
+        @IntRange(from=1, to=1) int e = (i %= 2);
+    }
+}
--- tests/intrange/Unaries.java
+++ tests/intrange/Unaries.java
@@ -10,21 +10,6 @@ class Unaries {
         @IntRange(from=-11, to=0) int d = ~a;
     }
     
-    public void postfix() {
-        int i = 3;
-        @IntRange(from=3, to=3) int a = i++;
-        @IntRange(from=4, to=4) int b = i;
-        @IntRange(from=4, to=4) int c = i--;
-        
-    }
-    
-    public void prefix() {
-        int i = 3;
-        @IntRange(from=4, to=4) int a = ++i;
-        @IntRange(from=4, to=4) int b = i;
-        @IntRange(from=3, to=3) int c = --i;
-    }
-    
     public void unaryPlusMinus(@IntRange(from=-2, to=10) int a) {
         @IntRange(from=-2, to=10) int b = +a;
         @IntRange(from=-10, to=2) int c = -a;
