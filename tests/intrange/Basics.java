5f3e245222f52e2f4177e378b76e2045f8406d4b
JasonMrX <jiasenxu@gmail.com>
2016-08-19 17:18:28 -0400

    fix Basic.java test case

--- tests/intrange/Basics.java
+++ tests/intrange/Basics.java
@@ -5,12 +5,9 @@ import intrange.qual.*;
 class Basics {
 
     public void IntegerTest() {
-        Integer d = new Integer(1000);
-        int a = d;
-//        if (true) {
-//            a = 2;
-//        }
-        @IntRange(from=0, to=2) Integer b = a;
+        Integer d = new Integer(0);
+
+        @IntRange(from=0, to=2) Integer b = d;
 
         //:: error: (assignment.type.incompatible)
         @IntRange(from=1, to=1) Integer c = d;
